c09f5666e1206bfd1f996d34dd78c46dde4f5477
Kerri <gant.kerri@gmail.com>
2021-01-23 00:15:51 -0500

    Database ready for migration and upgrade

--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,10 @@ class User(db.Model, UserMixin):
   username = db.Column(db.String(40), nullable = False, unique = True)
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False)
+  songs = db.relationship('Song', backref='user', lazy=True)
+  annotations = db.relationship('Annotation', backref='user', lazy=True)
+  comments = db.relationship('Comment', backref='user', lazy=True)
+  votes = db.relationship('Vote', backref='user', lazy=True)
 
 
   @property
@@ -31,3 +35,46 @@ class User(db.Model, UserMixin):
       "username": self.username,
       "email": self.email
     }
+
+
+class Song(db.Model):
+  __tablename__ = "songs"
+
+  id = db.Column(db.Integer, primary_key = True)
+  artist = db.Column(db.String(255), nullable=False)
+  title = db.Column(db.String(255), nullable=False)
+  album = db.Column(db.String(255), nullable=False)
+  lyrics = db.Column(db.Text, nullable=False)
+  song_url = db.Column(db.Text)
+  media_url = db.Column(db.Text)
+  user_Id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  annotations = db.relationship('Annotation', backref='song', lazy=True)
+  comments = db.relationship('Comment', backref='song', lazy=True)
+  votes = db.relationship('Vote', backref='song', lazy=True)
+
+
+class Annotation(db.Model):
+  __tablename__ = "annotations"
+
+  id = db.Column(db.Integer, primary_key = True)
+  song_facts = db.Column(db.Text, nullable=False)
+  user_Id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  song_Id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
+
+
+class Comment(db.Model):
+  __tablename__ = "comments"
+
+  id = db.Column(db.Integer, primary_key = True)
+  user_comment = db.Column(db.Text, nullable=False)
+  user_Id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  song_Id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
+
+
+class Vote(db.Model):
+  __tablename__ = "votes"
+
+  id = db.Column(db.Integer, primary_key = True)
+  votes = db.Column(db.Integer, nullable=False)
+  user_Id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  song_Id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
