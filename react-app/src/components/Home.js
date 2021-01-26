5f88ef92fac0863a5702e0f9189a9dbc4b5f9123
Safiya Cain <say.cain3@gmail.com>
2021-01-26 16:01:05 -0500

    create charts header

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,11 +1,14 @@
 import './styles/navbar.css';
+import './styles/home.css';
 import React from 'react';
 
 const HomePage = () => {
     return(
-        <div>
-            <h1> Charts </h1>
-            <h3> Trending on Cheongjae</h3>
+        <div className="song-charts">
+            <div className="chart-headings">
+                <h1 className="charts-title"> CHARTS </h1>
+                <h3> TRENDING ON CHEONJAE</h3>
+            </div>
         </div>
     )
 }
