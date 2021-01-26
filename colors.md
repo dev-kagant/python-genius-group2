c1449420b91fcd873dd7a0bb3e0f82780360712c
Damien Darko <warwickjunglex@gmail.com>
2021-01-26 18:22:30 -0500

    Navbar styles good

---
+++ colors.md
@@ -0,0 +1,9 @@
+Colors
+---
+Inchworm(darker): #C2FF88  
+Tea Green(lighter): #E1FFC4  
+White: #FFFFFF  
+Medium Champagne(lighter): #F7E6A0  
+Jonquil(darker): #EFCC41  
+
+![color palette](color-palette.png)
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,17 +6,19 @@ import './styles/navbar.css';
 const NavBar = ({ setAuthenticated }) => {
   return (
     <nav className='navbar-container'>
-      <div >
+      <div>
         <ul className='navbar-inner-container1'>
           <li>
-            <input type="search" placeholder="Search bar" />
+            <form>
+              <input className='navbar-search' type="search" placeholder="Search bar" /><img className='search-icon' src="https://img.icons8.com/fluent-systems-filled/24/000000/search.png" />
+            </form>
           </li>
           <li>
             <NavLink to="/" exact={true} activeClassName="active" className="home-link">
               천재
             </NavLink>
           </li>
-          <div>
+          <div className='navbar1-links'>
             <li>
               <NavLink to="/login" exact={true} activeClassName="active" className="login-link">
                 Login
@@ -33,17 +35,17 @@ const NavBar = ({ setAuthenticated }) => {
               </NavLink>
             </li>
             <li>
-              <LogoutButton setAuthenticated={setAuthenticated} />
+              <LogoutButton className='logout' setAuthenticated={setAuthenticated} />
             </li>
           </div>
         </ul>
       </div>
       <div className='navbar-inner-container2'>
-        <NavLink to="/charts" className="second-nav-links" exact={true} activeClassName="active">
-          CHARTS
+        <NavLink to="/charts" className="second-nav-links navbar-charts" exact={true} activeClassName="active">
+          Charts
         </NavLink>
-        <NavLink to="/add-a-song" className="second-nav-links" exact={true} setAuthenticated={setAuthenticated}>
-          ADD A SONG
+        <NavLink to="/add-a-song" className="second-nav-links navbar-add-a-song" exact={true} setAuthenticated={setAuthenticated}>
+          Add A Song
         </NavLink>
       </div>
     </nav>
