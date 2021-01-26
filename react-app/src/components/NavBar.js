226eebc6b98611710aa7cff84f816e93091d1e16
Damien Darko <warwickjunglex@gmail.com>
2021-01-25 18:58:23 -0800

    Began navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './styles/navbar.css'; 
 
 const NavBar = ({ setAuthenticated }) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
+    <nav className='navbar-container'>
+      <div className='navbar-inner-container1'>
+        <ul>
+          <li>
+            <NavLink to="/" exact={true} activeClassName="active">
+              천재 Cheonjae
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/users" exact={true} activeClassName="active">
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton setAuthenticated={setAuthenticated} />
+          </li>
+        </ul>
+      </div>
+      <div className='navbar-inner-container2'>
+        <NavLink to="/charts" exact={true} activeClassName="active">
+          Charts
+        </NavLink>
+        <NavLink to="/add-a-song" exact={true} setAuthenticated={setAuthenticated}>
+          Add A Song
+        </NavLink>
+      </div>
     </nav>
   );
 }
