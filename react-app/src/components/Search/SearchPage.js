b90d5158d9169c7e894ba54f86771a34331657d5
Safiya Cain <say.cain3@gmail.com>
2021-01-28 18:21:26 -0500

    add classes to lyrics

--- react-app/src/components/Search/SearchPage.js
+++ react-app/src/components/Search/SearchPage.js
@@ -12,31 +12,31 @@ function SearchPage(){
             <ul className="search-page_songInfo"> Songs {
                 songInfo && songInfo.map((song)=>(
                     <div className="search-song_data" key={song.title}>
-                        <li><img className="song-img" src={song.song_icon} /></li>
-                        <h1>{song.title}</h1>
-                        <h1>{song.artist}</h1>
+                        <li><img className="song-img" src={song.song_icon} alt={song.title}/></li>
+                        <div className="song-title"><li>{song.title}</li></div>
+                        <div className="song-artist"><li>{song.artist}</li></div>
                     </div>
                 ))
             }</ul>
-            <div className="artistInfo"> Artists {
+            <ul className="search-page_artistInfo"> Artists {
                 artistInfo && artistInfo.map((artist)=> (
-                    <div key={artist.artist}>
-                        <img src={artist.song_icon} />
-                        <h1>{artist.artist}</h1>
+                    <div className="search-artist_data" key={artist.artist}>
+                        <li><img className="artist-img" src={artist.song_icon} /></li>
+                        <div className="artist-name"><li>{artist.artist}</li></div>
                     </div>
                 ))
-            }</div>
-            <div className="lyricsInfo"> Lyric Matches{
+            }</ul>
+            <ul className="search-page_lyricsInfo"> Lyric Matches{
                 lyricsInfo && lyricsInfo.map((lyrics)=>(
                     <div key={lyrics.lyrics}>
-                        <img src={lyrics.song_icon} />
-                        <h1>{lyrics.title}</h1>
-                        <h1>{lyrics.artist}</h1>
-                        <h1>{lyrics.album}</h1>
-                        <p>{lyrics.lyrics}</p>
+                        <li><img className="song-img" src={lyrics.song_icon} /></li>
+                        <div className="song-title"><li>{lyrics.title}</li></div>
+                        <div className="song-lyrics"><li>{lyrics.artist}</li></div>
+                        <div className="album-name"><li>{lyrics.album}</li></div>
+                        <div><li><p>{lyrics.lyrics}</p></li></div>
                     </div>
                 ))
-            }</div>
+            }</ul>
         </div>
     )
 }
