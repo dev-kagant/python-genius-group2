975d36d0b203c98444987c6ffa0bb60b89a9acc3
Safiya Cain <say.cain3@gmail.com>
2021-01-29 10:44:55 -0500

    add links to search page

--- react-app/src/components/Search/SearchPage.js
+++ react-app/src/components/Search/SearchPage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {useHistory} from "react-router-dom";
 import "./styles/search.css";
 
 function SearchPage(){
+    const history = useHistory();
+
     const songInfo = useSelector(state => state.search.song);
     const artistInfo = useSelector(state => state.search.artist);
     const lyricsInfo = useSelector(state => state.search.lyrics);
@@ -11,7 +14,7 @@ function SearchPage(){
         <div className="search-page_container">
             <ul className="search-page_songInfo"> Songs {
                 songInfo && songInfo.map((song)=>(
-                    <div className="search-song_data" key={song.title}>
+                    <div className="search-song_data" key={song.title} onClick={()=>history.push(`songs/${song.id}`)}>
                         <li><img className="song-img" src={song.song_icon} alt={song.title}/></li>
                         <div className="song-title"><li>{song.title}</li></div>
                         <div className="song-artist"><li>{song.artist}</li></div>
@@ -20,7 +23,7 @@ function SearchPage(){
             }</ul>
             <ul className="search-page_artistInfo"> Artists {
                 artistInfo && artistInfo.map((artist)=> (
-                    <div className="search-artist_data" key={artist.artist}>
+                    <div className="search-artist_data" key={artist.artist} onClick={()=>history.push(`songs/${artist.id}`)}>
                         <li><img className="artist-img" src={artist.song_icon} /></li>
                         <div className="artist-name"><li>{artist.artist}</li></div>
                     </div>
@@ -28,7 +31,7 @@ function SearchPage(){
             }</ul>
             <ul className="search-page_lyricsInfo"> Lyric Matches{
                 lyricsInfo && lyricsInfo.map((lyrics)=>(
-                    <div className="search-lyrics_data" key={lyrics.lyrics}>
+                    <div className="search-lyrics_data" key={lyrics.lyrics} onClick={()=>history.push(`songs/${lyrics.id}`)}>
                         <li><img className="song-img" src={lyrics.song_icon} /></li>
                         <div className="song-title"><li>{lyrics.title}</li></div>
                         <div className="song-lyrics"><li>{lyrics.artist}</li></div>
