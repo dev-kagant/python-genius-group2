89e0e57d291667656901e7ed27eaea04d3314c7b
Kimi Zou <kimizou.kz@gmail.com>
2021-03-05 16:40:36 -0500

    Refactor footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,59 +1,51 @@
 import React from 'react';
-
-import angellist from './icons/angellist.png';
-import linkedin from './icons/linkedin.png';
-
+import Member from './Member';
 import './Footer.css';
 
-function Footer() {
-    return (
-        <div className='footer-container'>
-            <div className='ashley-links'>
-                <a className='about-link' href='https://angel.co/u/kerri-gant' rel="noreferrer" target="_blank">
-                    <img src={angellist} alt='angellist' />
-                </a>
-                <a className='ashley-link' href='https://github.com/dev-kagant' rel="noreferrer" target="_blank">
-                    Ashley Gant
-                </a>
-                <a className='about-link' href='https://www.linkedin.com/in/kerrigant/' rel="noreferrer" target="_blank">
-                    <img src={linkedin} alt='linkedin' />
-                </a>
-            </div>
-            <div className='safiya-links'>
-                <a className='about-link' href='https://angel.co/u/safiya-cain' rel="noreferrer" target="_blank">
-                    <img src={angellist} alt='angellist' />
-                </a>
-                <a className='safiya-link' href='https://github.com/Scain3' rel="noreferrer" target="_blank">
-                    Safiya Cain
-                </a>
-                <a className='about-link' href='https://www.linkedin.com/in/safiya-cain-25327356/' rel="noreferrer" target="_blank">
-                    <img src={linkedin} alt='linkedin' />
-                </a>
-            </div>
-            <div className='kimi-links'>
-                <a className='about-link' href='https://angel.co/u/kimi-zou' rel="noreferrer" target="_blank">
-                    <img src={angellist} alt='angellist' />
-                </a>
-                <a className='kimi-link' href='https://github.com/Kimi-Zou' rel="noreferrer" target="_blank">
-                    Kimi Zou
-                </a>
-                <a className='about-link' href='https://www.linkedin.com/in/kimizou/' rel="noreferrer" target="_blank">
-                    <img src={linkedin} alt='linkedin' />
-                </a>
-            </div>
-            <div className='damien-links'>
-                <a className='about-link' href='https://angel.co/u/damien-darko' rel="noreferrer" target="_blank">
-                    <img src={angellist} alt='angellist' />
-                </a>
-                <a className='damien-link' href='https://github.com/djangothesolarboy' rel="noreferrer" target="_blank">
-                    Damien Darko
-                </a>
-                <a className='about-link' href='https://www.linkedin.com/in/damien-darko/' rel="noreferrer" target="_blank">
-                    <img src={linkedin} alt='linkedin' />
-                </a>
-            </div>
+function Footer () {
+  return (
+    <div className='footer-container'>
+      <div className='footer__left'>
+        <div className='footer__about'>
+          천재 (CHEONJAE) IS A GENIUS CLONE CREATED
+          FOR PASSIONATE K-POP FANS.
         </div>
-    )
+        <div className='footer__copyright'>
+          © 2021 CHEONJAE GENIUS
+        </div>
+      </div>
+      <div className='footer__team'>
+        <div className='footer__team-col'>
+          <Member
+            name='Kerri Gant'
+            github='https://github.com/dev-kagant'
+            linkedin='https://www.linkedin.com/in/kerrigant/'
+            angellist='https://angel.co/u/kerri-gant'
+          />
+          <Member
+            name='Safiya Cain'
+            github='https://github.com/Scain3'
+            linkedin='https://www.linkedin.com/in/safiya-cain-25327356/'
+            angellist='https://angel.co/u/safiya-cain'
+          />
+        </div>
+        <div className='footer__team-col'>
+          <Member
+            name='Kimi Zou'
+            github='https://github.com/Kimi-Zou'
+            linkedin='https://www.linkedin.com/in/kimizou/'
+            angellist='https://angel.co/u/kimi-zou'
+          />
+          <Member
+            name='Damien Darko'
+            github='https://github.com/djangothesolarboy'
+            linkedin='https://www.linkedin.com/in/damien-darko/'
+            angellist='https://angel.co/u/damien-darko'
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
