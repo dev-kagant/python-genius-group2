6d3f7b0b86b95738fad29818444f63e4bc79448b
Kimi Zou <kimizou.kz@gmail.com>
2021-03-05 21:31:54 -0500

    Refactor charts

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import HomePage from "./components/HomePage/Home";
-import NavBar from "./components/NavBar/NavBar";
-import LoginModal from "./components/auth/LoginModal/LoginModal";
-import SignUpModal from "./components/auth/SignUpModal/SingUpModal";
-import UsersList from "./components/UsersList";
-import User from "./components/User/User";
-import Song from "./components/SongPage";
-import SongAddForm from "./components/SongPage/SongAddForm";
-import SearchPage from "./components/Search/SearchPage";
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
+import LoginModal from './components/auth/LoginModal/LoginModal';
+import SignUpModal from './components/auth/SignUpModal/SingUpModal';
+import UsersList from './components/UsersList';
+import User from './components/User/User';
+import Song from './components/SongPage';
+import SongAddForm from './components/SongPage/SongAddForm';
+import SearchPage from './components/Search/SearchPage';
 import Footer from './components/Footer/Footer';
 
-import { authenticate } from "./store/user";
-import SongTrends from "./components/HomePage/SongTrends";
-import Charts from "./components/Charts/Charts";
+import { authenticate } from './store/user';
 
-function App() {
+function App () {
   const dispatch = useDispatch();
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -37,43 +35,43 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route path="/login" exact={true}>
-          <HomePage />
+        <Route path='/login' exact>
+          <Home />
           <LoginModal />
         </Route>
-        <Route path="/sign-up" exact={true}>
-          <HomePage />
+        <Route path='/sign-up' exact>
+          <Home />
           <SignUpModal />
         </Route>
-        <ProtectedRoute path="/users" exact={true} >
-          <UsersList/>
+        <ProtectedRoute path='/users' exact>
+          <UsersList />
         </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true} >
+        <ProtectedRoute path='/users/:userId' exact>
           <User />
         </ProtectedRoute>
-        <Route path="/" exact={true} >
-          <HomePage />
+        <Route path='/' exact>
+          <Home />
         </Route>
-        <Route path="/charts">
-          <Charts />
+        <Route path='/charts'>
+          <Home />
         </Route>
-        <Route path="/songs/:songId" exact={true}>
+        <Route path='/songs/:songId' exact>
           <Song />
         </Route>
-        <ProtectedRoute path="/add-a-song" exact={true} >
+        <ProtectedRoute path='/add-a-song' exact>
           <SongAddForm />
         </ProtectedRoute>
-        <Route path="/search">
+        <Route path='/search'>
           <SearchPage />
         </Route>
-        <Route path="/404" exact={true}>
+        <Route path='/404' exact>
           <h1>Sorry Page Not Found.</h1>
         </Route>
         <Route>
           <h1>Sorry Page Not Found.</h1>
         </Route>
       </Switch>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
